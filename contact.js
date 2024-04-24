9cf2c761a27d09e4f6d094f0bbc24a59a5f68f96
David Flores <soydoft@gmail.com>
2024-04-24 18:40:21 -0400

    updated contact form and donate form

---
+++ contact.js
@@ -0,0 +1,14 @@
+
+
+function validateForm() {
+    let x = document.forms["contact-form"][fname].value;
+    if (x === "") {
+        alert("Name must be filled out.");
+    }
+}
+
+function validateNumber() {
+    if (isNaN(x)) {
+
+    }
+}
